7321bd6c9cd80834701e8f7ce5813859f2375746
korarit <krtza1@gmail.com>
2025-01-16 00:29:46 +0700

    migration db

---
+++ prisma/migrations/20250115150713_init/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `description` on the `booktype` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE `booktype` DROP COLUMN `description`;
