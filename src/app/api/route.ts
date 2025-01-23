ae3f6034ed93d0d84a99dc845225785af34e2920
thestepkla <koraritsw@gmail.com>
2025-01-23 13:50:57 +0700

    change group member

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -3,11 +3,11 @@ import { NextRequest, NextResponse } from "next/server";
 export function GET(request: NextRequest) {
 
     const listMembers = [
-        { studentId: "66024xxx", name: "TestAAA" },
+        { studentId: "66024xxx", name: "TestAAACS" },
         { studentId: "66024xxx", name: "TestCCC" },
         { studentId: "66024xxx", name: "TestBBB" }
     ];
 
-    return NextResponse.json({ message: "GET method called" });
+    return NextResponse.json({ message: "GET Group Member Success", data: listMembers });
 
 }
